ddbc2138bde39f60120d86a3fc82336c61d40283
Ravikiran2402 <ravikiranbudde@gmail.com>
2019-08-09 22:05:44 +0530

    Multiplication functionality updated

--- caladd.java
+++ caladd.java
@@ -23,11 +23,11 @@ public class caladd
                             System.out.print("Result = "+res);
                             break;
                 case '2':   System.out.print("Enter two numbers: ");
-                            // a = scan.nextFloat();
-                            // b = scan.nextFloat();
-                            // res = a*b;
-                            // System.out.print("Result = "+res);
-                            // break;
+                            a = scan.nextFloat();
+                            b = scan.nextFloat();
+                            res = a*b;
+                            System.out.print("Result = "+res);
+                            break;
                 case '3':   System.exit(0);
                             break;
                 default: System.out.print("Inavlid Choice!");
